ecfd305581b170e823b718adf011cdb5cd9e2b49
gdimitriu <gabriel.dimitriu@yahoo.com>
2021-05-27 04:20:24 -0700

    added a new test

--- kafka_app/src/test/resources/test.sh
+++ kafka_app/src/test/resources/test.sh
@@ -2,4 +2,5 @@
 curl -X POST -H "Content-Type:application/json" http://localhost:8080/kafka/client/topics/create -d @createTopic.json -u ad:ad
 curl -X POST -H "Content-Type:application/json" http://localhost:8080/kafka/client/topics/test -d @first.json -u ad:ad
 curl -X GET http://localhost:8080/kafka/client/topics/test/allrecords/myGroupId/myClientId -u ad:ad
+curl -X GET http://localhost:8080/kafka/client/topic/test/myconsumer/myid -u ad:ad
 curl -X DELETE http://localhost:8080/kafka/client/topics/test -u ad:ad
